5c28088281c36e08a2d4b018b3433d61be150de9
michael barrett <michaelbarrett343@googlemail.com>
2023-07-08 14:59:32 +0100

    added items page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,28 @@ class ItemsController < ApplicationController
     def index
       @items = Item.where(items: { user_id: current_user.id })
     end
+
+    def new
+      @item = Item.new
+    end
+  
+    def create
+      @Item = Item.new
+      if @event.save
+        # redirect_to profile_path
+      else
+        render :new
+      end
+    end
+  
+    def destroy
+      @Item.delete
+    end
+  
+  private
+   def set_user
+     if user_signed_in?
+      @current_user = current_user
+     end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
-  get 'users/:id/items' => 'items#index', :as => :user_items_path
+  get "items", to: "items#index"
+  #get 'users/:id/items' => 'items#index', :as => :user_items_path
+
   resources :user do
     resources :items
   end
