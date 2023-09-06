ca1bd7eca0f9279d34e36518d57bc1b926f0266e
michael barrett <michaelbarrett343@googlemail.com>
2023-09-06 13:53:23 +0100

    added event listener for input and clear for payment calculator

--- app/javascript/packs/paymentcalc.js
+++ app/javascript/packs/paymentcalc.js
@@ -1,1 +1,19 @@
-const calcBtn = document.getElementsByClassName('payment-calc');
+document.addEventListener("DOMContentLoaded", () => {
+  const calcBtn = document.getElementsByTagName("input");
+  const result = document.getElementById('result');
+  console.log(result);
+  for (var i = 0; i < calcBtn.length; i++) {
+  
+    calcBtn[i].addEventListener('click', (e) => {
+      console.log(e.target.value);
+      if (typeof(e.target.value == 'number')) {
+        console.log('is a number')
+        result.value += e.target.value;
+      }
+      if (e.target.value == 'C') {
+        result.value = "";
+      }
+    })
+  }
+})
+
